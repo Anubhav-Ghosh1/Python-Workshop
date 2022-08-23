7a9fea3cff23a767ca8302c256aaa8e60505693f
Anubhav Ghosh <88537612+Anubhav-Ghosh1@users.noreply.github.com>
2022-08-23 19:46:57 +0530

    Create intro.py

---
+++ For Loop/intro.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Loop's in python
+
+# In[2]:
+
+
+for i in range(1,5):
+    print(i)
+
+
+# Here i is an varible where it will increment till ,5 in this case
+# Here we have to initlize range in 
+# for variable_name in range(start,end,increment)
+# 
